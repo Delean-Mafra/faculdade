91d537a76fde60d6fcffae895dcd2150e815602d
Delean Mafra <170483770+Delean-Mafra@users.noreply.github.com>
2025-10-25 18:59:29 -0300

    Create Operações com Arrays e Matrizes usando NumPy.py

---
+++ Prática Integradora de Ciência de Dados com Cloud Computing/Operações com Arrays e Matrizes usando NumPy.py
@@ -0,0 +1,39 @@
+#______________é o que faz o o cógido, arr = np.arange(1, 11)
+# R: Array com números de 1 a 10 é o que faz o o cógido, arr = np.arange(1, 11)
+
+import numpy as np
+
+
+arr = np.arange(1, 11)
+
+
+print(f"{arr} é o que faz o o cógido, arr = np.arange(1, 11)")
+
+
+# ______________é o que faz o o cógido, matriz = np.arange(1, 13).reshape(3, 4)
+# R: Matriz bidimensional é o que faz o o cógido, matriz = np.arange(1, 13).reshape(3, 4)
+
+
+matriz = np.arange(1, 13).reshape(3, 4)
+
+print(f"{matriz} é o que faz o o cógido, matriz = np.arange(1, 13).reshape(3, 4)")
+
+
+# ______________é o que faz o código, print("Array 'arr':")
+
+# R: Imprime o array 'arr':" é o que faz o código, print("Array 'arr':")
+
+
+
+print("Array 'arr':","""é o que faz o código, print("Array 'arr':")""")
+
+
+
+# ____________é o que faz o código, print("\nMatriz 'matriz':")
+
+# R: Imprime a Matriz 'matriz' é o que faz o código, print("\nMatriz 'matriz':")
+
+
+print("\nMatriz 'matriz':",r""" é o que faz o código, print("\nMatriz 'matriz':")""")
+
+
