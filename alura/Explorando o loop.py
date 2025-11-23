f3f3cd8be0cb4e2bb287a7bf1755f8b13bc9abc0
Delean Mafra <170483770+Delean-Mafra@users.noreply.github.com>
2025-11-22 21:33:32 -0300

    Create Explorando o loop.py

---
+++ alura/Explorando o loop.py
@@ -0,0 +1,56 @@
+pessoa = {
+    'nome': 'João',
+    'idade': 30,
+    'altura': '1.75m'
+}
+
+# print(pessoa)
+
+
+for chave, valor in pessoa.items():
+    print(f"A chave é {chave} e o valor é {valor}")
+
+
+for n in range(11):
+    if n % 2 == 0:
+        print(f"O número {n} é par")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# z = 1
+
+# for n in range(1,100):
+#     if z == 1:
+#         print(z)
+#         z=z+z
+#     # elif z == 2:
+#     #     print(z)
+#     #     z=z*2
+#     else:
+        
+#         print(z)
+#         z=z*2
+
+# soma = 0
+# for n in range(1, 101):
+#     soma += n
+# print(soma)
+
+
