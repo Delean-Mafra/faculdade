1a4cb4af65adccc6e1896a86acb568dbc90c3fc5
Delean Mafra <170483770+Delean-Mafra@users.noreply.github.com>
2026-03-10 01:34:47 -0300

    Create Simple Array Sum.py

---
+++ hackerrank/Simple Array Sum.py
@@ -0,0 +1,31 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'simpleArraySum' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY ar as parameter.
+#
+
+def simpleArraySum(ar):
+    # soma todos os elementos do array e retorna o resultado
+    return sum(ar)
+
+if __name__ == '__main__':
+    fptr = open(os.environ.get('OUTPUT_PATH', '/dev/stdout'), 'w')
+
+    ar_count = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = simpleArraySum(ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
